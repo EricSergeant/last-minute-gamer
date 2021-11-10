0b220b02ad56c2e3ec1206437bdfa91a726eb7bb
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-11-10 11:04:56 -0600

    Verify ability to display info on screen

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       .then(data =>
         this.setState({
           // creatures: data.name
-          creatures: data.results
+          creatures: data.results[1].name
         })
       )
       .catch(error => {
@@ -30,13 +30,15 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h2>Found this:</h2>
-          {/* <p>{this.state.creatures.name}</p> */}
-          {console.log('in render:', this.state.creatures)}
+      <div className="app">
+        <header className="app-header">
+          <h1 className="page-title">Find A Monster</h1>
         </header>
-        <main className="App-main">
+        <main className="app-main">
+          {this.state.hasError && <h2>This was an error summoning a creature from the server, please try again.</h2>}
+          <h2>Found this:</h2>
+          <p>{this.state.creatures}</p>
+          {console.log('in render:', this.state.creatures)}
 
         </main>
       </div>
