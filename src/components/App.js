110f7846db7e7b32fcc8aea9e2efda27776e528b
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-11-10 10:53:59 -0600

    Create initial render for app

--- src/components/App.js
+++ src/components/App.js
@@ -30,29 +30,18 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Found this:</h2>
-        {/* <p>{this.state.creatures.name}</p> */}
-        {console.log('in render:', this.state.creatures)}
+      <div className="App">
+        <header className="App-header">
+          <h2>Found this:</h2>
+          {/* <p>{this.state.creatures.name}</p> */}
+          {console.log('in render:', this.state.creatures)}
+        </header>
+        <main className="App-main">
+
+        </main>
       </div>
     )
   }
 }
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-      </header>
-    </div>
-  );
-}
-*/
 
 export default App;
