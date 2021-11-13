3397587389bcba17810c24a2f35b13dba3d6482b
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-11-13 16:24:46 -0600

    Add error page handling for incorrect URL

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import NavBar from './NavBar';
 import Error from './Error';
 import Favorites from './Favorites';
 import '../styles/castle.jpg';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -18,48 +18,50 @@ const App = () => {
       </header>
       <main className="app-main">
         {/* {this.state.hasError && <h2>This was an error summoning a creature from the server, please try again.</h2>} */}
-        {/* <Switch> */}
-        <Route exact path="/" render={() =>
-          <div>
-            <Main />
-          </div>
-        }
-        />
+        <Switch>
+          <Route exact path="/" render={() =>
+            <div>
+              <Main />
+            </div>
+          }
+          />
 
-        <Route exact path="/creatures/:CR" render={(routeProps) => {
-          console.log('CR routeProps', routeProps);
-          return (
-            < CreatureContainer creatureNumber={routeProps.match.params.CR} />
-          )
-        }
-        }
-        />
+          <Route exact path="/creatures/:CR" render={(routeProps) => {
+            console.log('CR routeProps', routeProps);
+            return (
+              < CreatureContainer creatureNumber={routeProps.match.params.CR} />
+            )
+          }
+          }
+          />
 
-        <Route exact path="/creatures/:Creature/details" render={(detailProps) => {
-          console.log('details routeProps', detailProps);
-          return (
-            < CreatureDetail creatureDetail={detailProps.match.params.Creature} />
-          )
-        }
-        }
-        />
+          <Route exact path="/creatures/:Creature/details" render={(detailProps) => {
+            console.log('details routeProps', detailProps);
+            return (
+              < CreatureDetail creatureDetail={detailProps.match.params.Creature} />
+            )
+          }
+          }
+          />
 
-        <Route exact path="/favorites" render={() =>
-          <Favorites />}
-        />
+          <Route exact path="/favorites" render={() =>
+            <Favorites />}
+          />
 
-        <Route path='/error' render={() =>
-          <Error />}
-        />
+          <Route path='/error' render={() =>
+            <Error />}
+          />
+
+          <Route component={Error} />
 
 
-        {/* 
+          {/* 
           The code below works but flashes error on each page load:
           <Route path="*" render={() =>
             <Error />}
           /> */}
 
-        {/* </Switch> */}
+        </Switch>
       </main>
     </div>
   )
