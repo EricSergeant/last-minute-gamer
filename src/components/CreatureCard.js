b23cbaedb4c0f3a3ca97a12e3abb18fef2202d51
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-11-15 11:33:26 -0600

    Correct creatureCard file

--- src/components/CreatureCard.js
+++ src/components/CreatureCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/CreatureCard.css';
 import { Link } from 'react-router-dom';
 
-const CreatureCard = ({ index, name, url, CR }) => {
+const CreatureCard = ({ index, name }) => {
   return (
     <Link to={`/creatures/${index}/details`}>
       <p className="card-display"> <br></br> {name} <br></br>
