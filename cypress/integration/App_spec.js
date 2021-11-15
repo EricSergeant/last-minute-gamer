92f2ad80024fe2dda6855fed1d22e7ae4cbae357
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-11-14 18:59:12 -0600

    Passing tests on main page view

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -2,9 +2,15 @@ beforeEach(() => {
   cy.visit('http://localhost:3000/');
 });
 
-describe('App should load', () => {
-  it('SHould confirm true is true', () => {
-    expect(true).to.equal(true)
+describe('App should load main page', () => {
+  it('Should display the main app page', () => {
+    cy.contains('Find A Monster')
+    cy.get('.main-text').contains('Running late to your D&D game?')
+  });
+  it('External links should not be undefined', () => {
+    cy.contains('a').should('not.have.attr', 'href', 'undefined')
   });
 })
+
+
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ const Main = () => {
       <h4>What exactly is a CR and how do I use it?</h4>
       <p>As of the 5th edition of Dungeons and Dragons, CR is a rating system that estimates the threat level of enemies. As a rule of thumb, a party of four characters can defeat an enemy of CR equal to their level without significant trouble.</p>
       <p>CR is also used to track experience and can be used to guide leveling up of characters.</p>
-      <p className="main-text">For a more detailed description, grab a copy of the <a href="https://dnd.wizards.com/products/dungeon-masters-guide" target="_blank" rel="noreferrer">Dungeon Master's Guide</a> or check out official information <a href="https://www.dndbeyond.com/sources/basic-rules/monsters#Challenge" target="_blank" rel="noreferrer">from DNDBeyond</a> regarding CR.</p>
+      <p className="main-text">For a more detailed description, grab a copy of the <a href="https://dnd.wizards.com/products/dungeon-masters-guide" id='DMG' target="_blank" rel="noreferrer">Dungeon Master's Guide</a> or check out official information <a href="https://www.dndbeyond.com/sources/basic-rules/monsters#Challenge" id="DNDB" target="_blank" rel="noreferrer">from DNDBeyond</a> regarding CR.</p>
       <p>This page utilizes the open API from https://www.dnd5eapi.co/api/</p>
     </div>
   )
