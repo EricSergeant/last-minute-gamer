7f24bbea41c39749d599ba19819707447b75fc5c
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-11-14 19:15:04 -0600

    Complete app page testing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
-import React, { Dropdown, DropdownMenu, DropdownItem } from 'react';
+import React from 'react';
 import '../styles/NavBar.css';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <div className="nav">
-      <NavLink to='/'>Home</NavLink>
+      <NavLink className='home-nav' to='/'>Home</NavLink>
       {/* <NavLink to='/creatures/allCR'>allCRs</NavLink> */}
-      <NavLink to='/favorites'>Favs</NavLink>
+      <NavLink id='favorites' to='/favorites'>Favs</NavLink>
       <NavLink to='/creatures/1'>CR1</NavLink>
       <NavLink to='/creatures/2'>CR2</NavLink>
       <NavLink to='/creatures/3'>CR3</NavLink>
