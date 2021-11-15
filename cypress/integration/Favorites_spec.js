4194e45094546c349b9633e592fb9d28f7e82d4b
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-11-15 11:17:02 -0600

    Add additional click test on favorites

--- cypress/integration/Favorites_spec.js
+++ cypress/integration/Favorites_spec.js
@@ -105,9 +105,9 @@ describe('Favorites should be recorded', () => {
   it('Should navigate to creature, add to favorites', () => {
     cy.get('.CR3-nav').click()
     cy.get('.creature-container').contains('Manticore')
-
     cy.visit('http://localhost:3000/creatures/manticore/details')
     cy.get('.add-fav-btn').click()
+    cy.get('.add-fav-btn').click() //checks that isn't added twice
     cy.get('.fav-nav').click()
     cy.get('.all-favs').contains('Manticore')
 
