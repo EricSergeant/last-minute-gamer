f4853146009b25b73bf814b18ae18cc634fc1aa4
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-11-14 19:41:28 -0600

    Pass all NavBar tests

--- cypress/integration/NavBar_spec.js
+++ cypress/integration/NavBar_spec.js
@@ -9,6 +9,76 @@ describe('NavBar should correctly route', () => {
     cy.get('.home-nav').click()
     cy.get('.main-text').contains('Click on a Challenge Rating(CR)')
   });
+  it('Should display the CR1 creatures', () => {
+    cy.get('.CR1-nav').click()
+    cy.get('.creature-container').contains('Bugbear')
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/creatures/1')
+    })
+  });
+  it('Should display the CR2 creatures', () => {
+    cy.get('.CR2-nav').click()
+    cy.get('.creature-container').contains('Gelatinous Cube')
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/creatures/2')
+    })
+  });
+  it('Should display the CR3 creatures', () => {
+    cy.get('.CR3-nav').click()
+    cy.get('.creature-container').contains('Bearded Devil')
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/creatures/3')
+    })
+  });
+  it('Should display the CR4 creatures', () => {
+    cy.get('.CR4-nav').click()
+    cy.get('.creature-container').contains('Succubus/Incubus')
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/creatures/4')
+    })
+  });
+  it('Should display the CR5 creatures', () => {
+    cy.get('.CR5-nav').click()
+    cy.get('.creature-container').contains('Unicorn')
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/creatures/5')
+    })
+  });
+  it('Should display the CR6 creatures', () => {
+    cy.get('.CR6-nav').click()
+    cy.get('.creature-container').contains('Young Brass Dragon')
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/creatures/6')
+    })
+  });
+  it('Should display the CR7 creatures', () => {
+    cy.get('.CR7-nav').click()
+    cy.get('.creature-container').contains('Shield Guardian')
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/creatures/7')
+    })
+  });
+  it('Should display the CR8 creatures', () => {
+    cy.get('.CR8-nav').click()
+    cy.get('.creature-container').contains('Frost Giant')
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/creatures/8')
+    })
+  });
+  it('Should display the CR9 creatures', () => {
+    cy.get('.CR9-nav').click()
+    cy.get('.creature-container').contains('Young Silver Dragon')
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/creatures/9')
+    })
+  });
+  it('Should display the CR10 creatures', () => {
+    cy.get('.CR10-nav').click()
+    cy.get('.creature-container').contains('Guardian Naga')
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/creatures/10')
+    })
+  });
 
 
 })
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,16 +8,16 @@ const NavBar = () => {
       <NavLink className='home-nav' to='/'>Home</NavLink>
       {/* <NavLink to='/creatures/allCR'>allCRs</NavLink> */}
       <NavLink className='fav-nav' id='favorites' to='/favorites'>Favs</NavLink>
-      <NavLink to='/creatures/1'>CR1</NavLink>
-      <NavLink to='/creatures/2'>CR2</NavLink>
-      <NavLink to='/creatures/3'>CR3</NavLink>
-      <NavLink to='/creatures/4'>CR4</NavLink>
-      <NavLink to='/creatures/5'>CR5</NavLink>
-      <NavLink to='/creatures/6'>CR6</NavLink>
-      <NavLink to='/creatures/7'>CR7</NavLink>
-      <NavLink to='/creatures/8'>CR8</NavLink>
-      <NavLink to='/creatures/9'>CR9</NavLink>
-      <NavLink to='/creatures/10'>CR10</NavLink>
+      <NavLink className='CR1-nav' to='/creatures/1'>CR1</NavLink>
+      <NavLink className='CR2-nav' to='/creatures/2'>CR2</NavLink>
+      <NavLink className='CR3-nav' to='/creatures/3'>CR3</NavLink>
+      <NavLink className='CR4-nav' to='/creatures/4'>CR4</NavLink>
+      <NavLink className='CR5-nav' to='/creatures/5'>CR5</NavLink>
+      <NavLink className='CR6-nav' to='/creatures/6'>CR6</NavLink>
+      <NavLink className='CR7-nav' to='/creatures/7'>CR7</NavLink>
+      <NavLink className='CR8-nav' to='/creatures/8'>CR8</NavLink>
+      <NavLink className='CR9-nav' to='/creatures/9'>CR9</NavLink>
+      <NavLink className='CR10-nav' to='/creatures/10'>CR10</NavLink>
     </div>
   )
 }
